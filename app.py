de9d334f138a8f4f0c77d9f3c34407d3a324a813
IDGS-803-22000965 <79228@alumnos.utleon.edu.mx>
2025-03-03 20:29:58 -0600

    Tabla de la base de datos

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_wtf.csrf import CSRFProtect
+from config import DevelopmentConfig
+from models import db, Alumnos
+import forms
 
 app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+csrf = CSRFProtect(app)
+
+db.init_app(app)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 @app.route("/")
 @app.route("/index")
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+    app.run()
---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from sqlalchemy import create_engine
+
+import urllib
+
+class Config(object):
+    SECRET_KEY ="Clave unica"
+    SESION_COOKIE_NAME = False
+    
+    
+class DevelopmentConfig(Config):    
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@localhost:3306/flaskbdidgs803'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
